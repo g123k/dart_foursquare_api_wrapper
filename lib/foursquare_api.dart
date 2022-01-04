ab161d43cb08cddb5231a75715e83729eaac92d7
Edouard Marquez <moi@edouard-marquez.me>
2022-01-04 08:55:33 +0100

    Base project

---
+++ lib/foursquare_api.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library foursquare_api;
+
+export 'src/foursquare_api_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
---
+++ test/foursquare_api_test.dart
@@ -0,0 +1,16 @@
+import 'package:foursquare_api/foursquare_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
